2c6f32a0c7dfd6ad0acf042159ae7987f917dff4
Yohan <yohan@yohanpro.com>
2020-12-04 21:48:43 +0900

    init

---
+++ src/components/Congrats/Congrats.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+import Congrats from '../Congrats';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+
+test('render Congrats Components', () => {
+
+});
---
+++ src/components/Congrats/index.js
@@ -0,0 +1,9 @@
+
+
+const Congrats = () => {
+    return (
+        <div></div>
+    );
+};
+
+export default Congrats;

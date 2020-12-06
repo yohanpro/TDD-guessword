00bae8eb31bdbb9ccdd7f80c2d4ae023d328f9d4
Yohan <yohan@yohanpro.com>
2020-12-06 15:52:15 +0900

    setupTests파일에 enzyme 설정 분리

--- src/components/Congrats/Congrats.test.js
+++ src/components/Congrats/Congrats.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { shallow } from 'enzyme';
+
 
 import Congrats from '../Congrats';
 import { findByTestAttr, checkProps } from '../../tests/testUtil';
 
-Enzyme.configure({ adapter: new EnzymeAdapter() });
+
 
 const defaultProps = { success: false };
 

798968930b9352979ab32d30f67ba2655b363b33
Yohan <yohan@yohanpro.com>
2020-12-20 23:01:19 +0900

    axios moxios 설치

---
+++ src/actions/actions.test.js
@@ -0,0 +1,16 @@
+import moxios from 'moxios';
+
+
+
+describe('getSecretWord action creater', () => {
+
+    beforeEach(() => {
+        moxios.install();
+    });
+
+    afterEach(() => {
+        moxios.uninstall();
+    });
+
+
+});
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,12 @@
 import { getLetterMatchCount } from '../helpers/index.js';
+import axios from 'axios';
+
 
 export const actionTypes = {
     CORRECT_GUESS: 'CORRECT_GUESS',
-    GUESS_WORD: 'GUESS_WORD'
+    GUESS_WORD: 'GUESS_WORD',
+    SET_SECRET_WORD: "SET_SECRET_WORD"
 };
-
 export const guessWord = (guessedWord) => {
     return function (dispatch, getState) {
         const { secretWord } = getState();
@@ -18,5 +20,12 @@ export const guessWord = (guessedWord) => {
         if (guessedWord === secretWord) {
             dispatch({ type: actionTypes.CORRECT_GUESS });
         }
+    };
+};
+
+//원래는 http 리퀘스트를 사용해서 가져와야 한다. async await 로 대기 해서 
+export const getSecretWord = () => {
+    return (dispatch) => {
+        axios.get('http://loaclhost:3030');
     };
 };
